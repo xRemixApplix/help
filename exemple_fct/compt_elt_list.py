87d75dd4f62cd933ec8f3978120ebffd977a9555
xRemixApplix <r3m1.appli@gmail.com>
2019-11-11 18:04:23 +0100

    Exemple de comptage d'un element donne dans une liste

---
+++ exemple_fct/compt_elt_list.py
@@ -0,0 +1,10 @@
+#-*- coding: utf8 -*-
+
+"""
+    Exemple de la fonction count() pour compter le nombre d'occurence d'un element dans une liste
+"""
+
+names = ["arthur", "jean", "eric", "arthur"]
+
+for elt in names :
+    print(names.count(elt))
